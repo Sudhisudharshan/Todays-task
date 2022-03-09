54a26386bf8a1ffb19a08839e08cee5d1219aeeb
sudhisudharshan <sudhisudahrshan86@gmail.com>
2022-03-09 10:37:21 +0530

    Assigment

---
+++ addice.sh
@@ -0,0 +1,7 @@
+clear
+dice1=$((random% 5 +1))
+dice2=$((random% 5 +1 ))
+sum=$((dice1+dice2))
+echo "$sum"
+
+
---
+++ conversion.sh
@@ -0,0 +1,7 @@
+clear 
+read -p "enter the feet;" x
+y=$((x*12))
+echo "$y"
+read -p "enter the inches;" a
+b=$((a/12))
+echo "$b"
