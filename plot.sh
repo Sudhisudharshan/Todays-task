91976bde9356d8b18c119ac71f0859181dc33e32
sudhisudharshan <sudhisudahrshan86@gmail.com>
2022-03-10 12:44:55 +0530

    text change

--- plot.sh
+++ plot.sh
@@ -4,6 +4,6 @@ div=3.2
 m=`echo $w $div | awk '{z= $1/$2; print z};'`
 m1=`echo $h $div | awk '{z=$1/$2; print z};'`
 sqm=`echo $m $m1 | awk '{print $1*$2}'`
-echo "area of rectangle is $sqm  square meter "
+echo "area of rectangle is = $sqm  square meter "
 acre=`echo $sqm $sq | awk '{print $1/$2};'`
 echo "conversion from square meter into acre" $acre 
