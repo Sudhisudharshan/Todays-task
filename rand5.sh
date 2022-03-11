a3021004cf9b9569e4c29d73626df21c0ba9752c
sudhisudharshan <sudhisudahrshan86@gmail.com>
2022-03-11 09:27:26 +0530

    Day 5

---
+++ rand5.sh
@@ -0,0 +1,16 @@
+a=$((RANDOM% 10))
+b=$((RANDOM% 10))
+c=$((RANDOM% 10))
+d=$((RANDOM% 10))
+e=$((RANDOM% 10))
+
+echo $a $b $c $d $e
+sum=$((a+b+c+d+e))
+echo $sum
+Average=$((sum/5))
+echo $Average 
+
+
+
+
+
